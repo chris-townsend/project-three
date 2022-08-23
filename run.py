806924dd8b0c0d9b1a742718c57c62ef3490b748
chris-townsend <chris-townsend@live.co.uk>
2022-08-23 12:23:32 +0000

    Initial commit - Add start_game function to print Chris's Hangman and add user input feild and greeting message to user

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def start_game():
+    """
+    Sets a function for when a user arrives on the start page,
+    asks the user for there name and welcomes user,
+    gives option 1 to play or 2 for instructions
+    """
+    print(
+        """
+           ___ _          _     _                      
+          / __\ |__  _ __(_)___( )__                   
+         / /  | '_ \| '__| / __|/ __|                  
+        / /___| | | | |  | \__ \\__ \                  
+        \____/|_| |_|_|  |_|___/|___/                  
+                                                   
+          /\  /\__ _ _ __   __ _ _ __ ___   __ _ _ __  
+         / /_/ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+        / __  / (_| | | | | (_| | | | | | | (_| | | | |
+        \/ /_/ \__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                           |___/                     
+        """                                           
+    )
+
+    name = input("Please Enter Your Name:\n")
+    print(f"Hello {name}, Welcome to Hangman and Good Luck!")
+
+
+print(start_game())
