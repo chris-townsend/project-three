01127d5c7655ad5a4152c8da16075bb1429d1c61
chris-townsend <chris-townsend@live.co.uk>
2022-08-31 09:55:37 +0000

    Delete/ Add whitespace needed within run.py file

--- run.py
+++ run.py
@@ -22,7 +22,6 @@ game_is_won = False
 def ask_for_name():
     while True:
         name = input(YELLOW + "Please Enter Your Name:\n")
-        
         if not name.isalpha():
             print("Name must be letters only\n")
         else:
@@ -51,20 +50,19 @@ def menu():
         elif user_input == "I":
             os.system("clear")
             header()
-            print( YELLOW +
-                "1.The computer will generate a random word and it's\n"
-                "your task to guess the letters from the word.\n"
-                "\n"
-                "2.To guess, type a letter of your choice and hit enter.\n"
-                "\n"
-                "3.If you guess correctly, the letter will be revealed.\n"
-                "\n"
-                "4.If you guess incorrectly, you will lose a life and \n"
-                " the Hangman will start to appear.\n"
-                "\n"
-                "5.You have 8 lives to guess the correct word.\n"
-    
-                "Good Luck!\n")
+            print(YELLOW +
+                  "1.The computer will generate a random word and it's\n"
+                  "your task to guess the letters from the word.\n"
+                  "\n"
+                  "2.To guess, type a letter of your choice and hit enter.\n"
+                  "\n"
+                  "3.If you guess correctly, the letter will be revealed.\n"
+                  "\n"
+                  "4.If you guess incorrectly, you will lose a life and \n"
+                  " the Hangman will start to appear.\n"
+                  "\n"
+                  "5.You have 8 lives to guess the correct word.\n"
+                  "Good Luck!\n")
             enter_input = input("Press Enter to go back to the menu\n").upper()
             if enter_input == "":
                 os.system("clear")
@@ -73,14 +71,13 @@ def menu():
                 break
             else:
                 print(RED + "Oops look's like you pressed the wrong key!\n")
-                
         else:
             print("Invalid Character, please try again!\n")
 
 
 def check_letter(letter, word):
     global reveal
-    for i in range(0,len(word)):
+    for i in range(0, len(word)):
         letter = word[i]
         if guess == letter:
             reveal[i] = guess
@@ -97,7 +94,7 @@ def restart_game():
     game_restart = False
 
     while not game_restart:
-        restart = input(YELLOW +"Would you like to play again?"
+        restart = input(YELLOW + "Would you like to play again?"
                         "Y/N").upper()
         try:
             if restart == "Y":
@@ -119,6 +116,7 @@ def restart_game():
 header()
 ask_for_name()
 
+
 def play():
     os.system("clear")
     header()
@@ -129,7 +127,7 @@ def play():
 
 while game_is_won == False and lives > 0:
     play()
-    guess = input(WHITE +'Guess a letter or an entire word:')
+    guess = input(WHITE + 'Guess a letter or an entire word:')
     guess = guess.upper()
 
     if guess == word:
@@ -139,41 +137,22 @@ while game_is_won == False and lives > 0:
         game_is_won = check_letter(guess, word)
     else:
         lives -= 1
-
     if game_is_won:
         print("\n")
         print(YELLOW + "W E L L  D O N E")
         player_won()
         print(f"you guessed the correct word ~ {word} with {lives} lives left")
         print(CYAN + "=============================================\n")
-        
     else:
         print("\n")
         print(RED + "Y O U  F A I L E D")
         player_lost()
         print(RED + f"you ran out of lives ~ the word was: {word}")
         print(CYAN + "=============================================\n")
-        
-    if game_is_won == True or lives <=0:        # Conditioned restart
+    if game_is_won == True or lives <= 0:        # Conditioned restart
         if restart_game() == True:
             game_is_won = False
             lives = 8
             word = random.choice(WORDS)
             word = word.upper()
             reveal = list(len(word)*'_')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
